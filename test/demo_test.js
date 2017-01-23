3a17175aa8bf6fb117b11c608c08cf1ebc61c86e
Alfredo Bollati <bollatialfredo@gmail.com>
2017-01-23 12:12:46 -0300

    mocha tests

---
+++ test/demo_test.js
@@ -0,0 +1,8 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+describe('some demo tests', function () {
+    it('adds two numbers together', function () {
+        assert(3 + 5 === 8);
+    })
+});
